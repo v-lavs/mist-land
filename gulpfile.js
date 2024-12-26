ab8e6d1d2d1ea0cea61b045041dfafaee15edcfd
v-lavs <lavskavanda@gmail.com>
2024-12-26 21:21:35 +0200

    change gulp config. added basic structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,104 @@
 'use strict';
 
-var gulp = require('gulp'),
-  watch = require('gulp-watch'),
-  prefixer = require('gulp-autoprefixer'),
-  uglify = require('gulp-uglify'),
-  sass = require('gulp-sass'),
-  sourcemaps = require('gulp-sourcemaps'),
-  cssmin = require('gulp-minify-css'),
-  include = require('gulp-include'),
-  gcmq = require('gulp-group-css-media-queries'),
-  rimraf = require('rimraf');
+const gulp = require('gulp'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    dartSass = require('sass'),
+    sass = require('gulp-sass')(dartSass),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssmin = require('gulp-minify-css'),
+    include = require('gulp-include'),
+    gcmq = require('gulp-group-css-media-queries'),
+    rimraf = require('rimraf'),
+    { promisify } = require('util');
 
-var path = {
-  build: {
-    js: './dist/',
-    css: './dist/'
-  },
-  src: {
-    js: './src/js/main.js',
-    style: './src/scss/main.scss'
-  },
-  watch: {
-    js: './src/js/**/*.js',
-    style: './src/scss/**/*.scss'
-  },
-  clean: './dist'
+const rimrafAsync = promisify(rimraf);
+
+const path = {
+    build: {
+        js: './dist/',
+        css: './dist/'
+    },
+    src: {
+        js: './src/js/main.js',
+        style: './src/scss/main.scss'
+    },
+    watch: {
+        js: './src/js/**/*.js',
+        style: './src/scss/**/*.scss'
+    },
+    clean: './dist'
 };
 
-gulp.task('clean', function (cb) {
-  rimraf(path.clean, cb);
-});
+// Clean task
+function clean() {
+    return rimrafAsync(path.clean);
+}
 
-gulp.task('js:dev', function () {
-  gulp.src(path.src.js)
-  .pipe(sourcemaps.init())
-    .pipe(include({
-      extensions: "js",
-      hardFail: true
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(path.build.js));
-});
+// JavaScript development task
+function jsDev() {
+    return gulp.src(path.src.js)
+        .pipe(sourcemaps.init())
+        .pipe(include({
+            extensions: "js",
+            hardFail: true
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.js));
+}
 
+// JavaScript build task
+function jsBuild() {
+    return gulp.src(path.src.js)
+        .pipe(include({
+            extensions: "js",
+            hardFail: true
+        }))
+        .pipe(gulp.dest(path.build.js));
+}
 
-gulp.task('js:build', function () {
-  gulp.src(path.src.js)
-    .pipe(include({
-      extensions: "js",
-      hardFail: true
-    }))
-    .pipe(gulp.dest(path.build.js));
-});
+// Styles development task
+function styleDev() {
+    return gulp.src(path.src.style)
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            sourceMap: true,
+            errLogToConsole: true
+        }))
+        .pipe(gcmq())
+        .pipe(gulp.dest(path.build.css));
+}
 
-gulp.task('style:dev', function () {
-  gulp.src(path.src.style)
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      sourceMap: true,
-      errLogToConsole: true
-    }))
-    .pipe(gcmq())
-    .pipe(gulp.dest(path.build.css));
-});
+// Styles build task
+function styleBuild() {
+    return gulp.src(path.src.style)
+        .pipe(sass({
+            sourceMap: false,
+            errLogToConsole: true
+        }))
+        .pipe(prefixer())
+        .pipe(gcmq())
+        .pipe(cssmin())
+        .pipe(gulp.dest(path.build.css));
+}
 
-gulp.task('style:build', function () {
-  gulp.src(path.src.style)
-    .pipe(sass({
-      sourceMap: false,
-      errLogToConsole: true
-    }))
-    .pipe(prefixer())
-    .pipe(gcmq())
-    .pipe(cssmin())
-    .pipe(gulp.dest(path.build.css));
-});
+// Watch tasks
+function watchStyles() {
+    gulp.watch(path.watch.style, styleDev);
+}
 
-gulp.task('build', [
-  'js:build',
-  'style:build'
-]);
+function watchJs() {
+    gulp.watch(path.watch.js, jsDev);
+}
 
-gulp.task('dev', [
-  'js:dev',
-  'style:dev'
-]);
+// Build and development tasks
+const build = gulp.series(clean, gulp.parallel(jsBuild, styleBuild));
+const dev = gulp.series(clean, gulp.parallel(jsDev, styleDev));
+const watch = gulp.parallel(watchStyles, watchJs);
 
-gulp.task('watch:styles', ['style:dev'], function () {
-  gulp.watch(path.watch.style, ['style:dev']);
-});
-
-gulp.task('watch:js', ['js:dev'], function () {
-  gulp.watch(path.watch.js, ['js:dev']);
-});
-
-gulp.task('watch',['watch:styles', 'watch:js'], function(){
-});
-
-gulp.task('default', ['build', 'watch']);
+// Default task
+gulp.task('clean', clean);
+gulp.task('build', build);
+gulp.task('dev', dev);
+gulp.task('watch', watch);
+gulp.task('default', gulp.series(dev, watch));
